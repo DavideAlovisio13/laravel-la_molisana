54a64d60655eba7021d1f43e102ae7f88b910651
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-21 10:32:51 +0200

    (about.blade) change folder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,11 @@
 
 @section('content')
 <section class="container">
-    <h1>Ciao</h1>
-    <p>Questa è l'home page</p>
+    <h1>I nostri prodotti</h1>
+    <ul>
+        @foreach ($products as $product)
+            <li>{{ $product['titolo']}}</li>
+        @endforeach
+    </ul>
 </section>
 @endsection
-
